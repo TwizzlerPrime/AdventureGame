e0906ab1adf54a4cddf4ba35c79e60698be04872
iremide <IAdewumi@smail.pembinatrails.ca>
2024-10-03 14:45:47 -0500

    Editing

--- game.sh
+++ game.sh
@@ -10,6 +10,13 @@ elif [ "$location" == "cave" ]; then
     cat cave.txt
     echo "But wait..."
     cat monster.txt
+    echo "What will you do?"
+    read action
+if ["$action" == "attack"]; then
+   echo "With which weapon?A sword, magic, or with intimidation?"
+   read weapon
+if ["$weapon" == "sword"]; then
+   echo "Your sword, broke,and the dragon was barely hurt, what now?"
 else
     echo "Invalid location."
 fi
